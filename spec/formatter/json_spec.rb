d905d00bd2159434c78a95c80cbd77fa8bc171c4
AkiraFukushima <h3.poteto@gmail.com>
2017-12-19 10:38:50 +0900

    Update travis config

--- spec/formatter/json_spec.rb
+++ spec/formatter/json_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Logist::Formatter::Json do
   context "message is a string" do
     let(:deserialized_output) { JSON.parse(formatter.call("debug", now, "", "test message")) }
     it do
-      expect(deserialized_output).to eq('level' => 'debug', 'environment' => 'development', 'timestamp' => now.strftime("%Y-%m-%dT%H:%M:%S.%6N "), 'message' => 'test message')
+      expect(deserialized_output).to eq('level' => 'debug', 'environment' => Rails.env, 'timestamp' => now.strftime("%Y-%m-%dT%H:%M:%S.%6N "), 'message' => 'test message')
     end
   end
 
   context "message is a json string" do
     let(:deserialized_output) { JSON.parse(formatter.call("debug", now, "", "{\"hoge\":\"fuga\"}")) }
     it do
-      expect(deserialized_output).to eq('level' => 'debug', 'environment' => 'development', 'timestamp' => now.strftime("%Y-%m-%dT%H:%M:%S.%6N "), 'hoge' => 'fuga')
+      expect(deserialized_output).to eq('level' => 'debug', 'environment' => Rails.env, 'timestamp' => now.strftime("%Y-%m-%dT%H:%M:%S.%6N "), 'hoge' => 'fuga')
     end
   end
 end
