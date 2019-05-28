36acdf9270b74210065cdd0f14af1b0266aebda9
Allan Batista <allan@allanbatista.com.br>
2019-05-28 12:16:24 -0300

    create new json parsers to json

--- lib/logist/formatter/json.rb
+++ lib/logist/formatter/json.rb
@@ -6,12 +6,21 @@ module Logist
   module Formatter
     class Json < ::Logger::Formatter
       def call(severity, timestamp, progname, msg)
-        begin
-          j = ::JSON.parse(msg)
-          temp = {level: severity, timestamp: format_datetime(timestamp), environment: ::Rails.env}.merge(j)
-          ::JSON.dump(temp) + "\n"
-        rescue JSON::ParserError
-          "{\"level\":\"#{severity}\",\"timestamp\":\"#{format_datetime(timestamp)}\",\"message\":\"#{msg}\",\"environment\":\"#{::Rails.env}\"}\n"
+        { level: severity, timestamp: format_datetime(timestamp), environment: ::Rails.env, message: format_message(msg) }.to_json + "\n"
+      end
+
+      def format_message(msg)
+        case msg.class.name
+          when "Hash"
+            msg
+          when "Array"
+            msg
+          else
+            begin
+              JSON.parse(msg)
+            rescue JSON::ParserError
+              "#{msg}"
+            end
         end
       end
     end
